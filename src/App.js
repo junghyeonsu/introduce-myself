4ff247edf71cec61bb7912e8478c4a196a50a5c2
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-12-22 17:45:35 +0900

    add header & border

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__container">
+        <div className="app__left__border" />
+        <div className="app__right__border" />
+        <div className="app__top__border" />
+        <span className="app__title">제 이름은 <span className="app__name">정현수</span>입니다.</span>
+      </div>
     </div>
   );
 }
